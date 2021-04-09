31201d0a2a3171aefecacc98910f20ee80434f3d
Fryles <mylesmarr@gmail.com>
2021-04-09 16:15:23 +0000

    added network

--- Gui.java
+++ Gui.java
@@ -1,5 +1,53 @@
-public class Gui() extends actionL{
-  JFrame myFrame = new JFrame();
+/**
+ * @Author Duffy Anderson, Lucius Holzinger, Myles Marr
+ * @Date 04/09/2021
+ * @Purpose: 
+ * @Update: 
+ * @Bugs: -
+ * 	    	-
+ * 	    	-
+ * */
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 
+
+public class Gui implements ActionListener extends Main{
+  String textAreaData;
+  JTextArea commentSection;
+  JFrame myFrame;
+
+  public Gui(){
+    myFrame  = new JFrame("First Frame");
+    commentSection = new JTextArea();
+    myFrame.setSize(300, 600);
+    myFrame.getDefaultCloseOperation();
+    myFrame.getContentPane().add(BorderLayout.SOUTH, button);
+  }
+
+  textAreaData = commentSection.getText();
+  
+  /**
+  * Should check for people interacting with the project
+  * might use a button but I would prefer something 
+  * similar to hyper links
+  */
+  public void ActionPreformed(ActionEvent e){
+    System.out.println("Action was preformed");
+  }
+
+  public JFrame algorithm(){
+    List<String> myAlgList = new ArrayList<>();
+    for(int i =0; ){
+      String parsedText;
+      parsedText = textAreaData.next();
+    }
+   
+  }
 }
---
+++ Network.java
@@ -0,0 +1,22 @@
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Network{
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("running");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://github.com/Fryles/Hack36"))
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
