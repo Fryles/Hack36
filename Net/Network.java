bd7357e76693a66dfe3a97f080b053660ed1858c
fryles <mylesmarr@gmail.com>
2021-04-09 11:34:43 -0700

    buffered img from base64

--- Net/Network.java
+++ Net/Network.java
@@ -1,12 +1,17 @@
 package Net;
 
-import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import Net.ImageHash;
+import java.io.*;
 import java.net.URI;
+import java.util.Base64;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
+import java.util.ArrayList;
 
 public class Network {
 
@@ -17,23 +22,24 @@ public class Network {
     }
 
     public static ImageHash[] get(String hashtag) {
-        BufferedImage[] imgs;
+        ArrayList<BufferedImage> imgs;
         try {
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://192.168.1.242/" + hashtag)).build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println(response.body());
-            
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public static BufferedImage base64StringToImg(final String base64String) {
-        try {
+    public static BufferedImage base64StringToImg(final String base64String){
+        try{
             return ImageIO.read(new ByteArrayInputStream(Base64.getDecoder().decode(base64String)));
-        } catch (final IOException ioe) {
-            throw new UncheckedIOException(ioe);
+        } catch(Exception e){
+            System.out.println(e);
+            //error, so return blank img
+            return new BufferedImage(200,200,BufferedImage.TYPE_INT_RGB);
         }
-    }
+        }
 }
