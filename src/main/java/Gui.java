ca131cfc1bc27a42a7da06a0b26a8ecb4aa6a1c0
Fryles <mylesmarr@gmail.com>
2021-04-10 17:17:26 +0000

    .

--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -91,7 +91,7 @@ public class Gui extends Main implements ActionListener {
     caption = new JTextArea();
     Color welcomeColor = new Color(180, 180, 180);
     caption.setBackground(welcomeColor);
-    //caption.setLayout();
+    caption.setPreferredSize(new Dimension(200, 30));
     caption.setSize(100,100);
     captionHead = new JLabel();
     postFrame.add(BorderLayout.SOUTH, caption);
